89d4d15b20a598ce1fc804828e87b6edd17e0ed2
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-29 14:43:01 -0700

    complete dsa stack lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,47 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        self.limit = limit
+        self.items = items
 
     def isEmpty(self):
-        pass
+        return self.items == []
 
     def push(self, item):
-        pass
+        if self.size() < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop()
+        return None
 
     def peek(self):
-        pass
-    
+        if self.items > 0:
+            return self.items[-1]
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if self.size() == self.limit:
+            return True
+        return False
 
     def search(self, target):
-        pass
+        distance = -1
+        search_items = self.items.copy()
+        i = 0
+        while search_items:
+            if search_items.pop() == target:
+                distance = i
+            i += 1
+        return distance
+
+    @property
+    def items(self):
+        return self._items
+
+    @items.setter
+    def items(self, items):
+        self._items = items[: self.limit]
